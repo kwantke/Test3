09aa4b13f7000802efdc39d2111a716a415a866a
kwantke <kokk7310@gamil.com>
2022-08-21 17:57:40 +0900

    Create Springboot

---
+++ src/main/java/com/example/test3/Test3Application.java
@@ -0,0 +1,16 @@
+package com.example.test3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test3Application {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("test");
+        SpringApplication.run(Test3Application.class, args);
+    }
+
+}
